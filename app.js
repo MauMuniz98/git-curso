71753de7296eccdda3441f812de9079b7667ffdb
mau <mau_md98git config --global user.email mau_md98>
2021-06-03 10:56:00 -0500

    finalizó la prueba de stash

---
+++ app.js
@@ -0,0 +1,6 @@
+var year= parseInt(prompt("inserte el año en el que nos encontramos",2021));
+
+console.log("nos encontramos en el año: ", year);
+document.write(" <h1>nos encontramos en el año: "+year+ "</h1>");
+
+
